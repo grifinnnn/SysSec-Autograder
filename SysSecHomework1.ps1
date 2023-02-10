855cae383780d01e69d0f688c59520713d62ce35
Griffin <104595294+grifinnnn@users.noreply.github.com>
2023-02-10 14:04:36 -0500

    revised homework 1 to remove boilerplate code

--- SysSecHomework1.ps1
+++ SysSecHomework1.ps1
@@ -3,10 +3,11 @@ $vSphereUser = Read-Host "vSphere Username"
 $vSpherePass = Read-Host "vSphere Password" -AsSecureString
 
 Connect-VIServer cdr-vcenter.cse.buffalo.edu -User $vSphereUser -Password $vSpherePass
+Clear-Host
 write-host("Grading Homework 1... This might take a while.")
 $startTime = Get-Date
 $ProgressPreference = 'SilentlyContinue'
-
+ 
 function InvokeScript {
     param(
         $SourceDevice,
@@ -27,61 +28,45 @@ function InvokeScript {
         }
         return Invoke-VMScript -ScriptText $Script -GuestPassword $Passwd -GuestUser $Username -VM $VM
         }
+    }
 
-}
-
+function CheckState {
+    param(
+        $SourceDevice,
+        $Script,
+        $Username,
+        $Passwd,
+        $TestName,
+        $TestExpectedResult,
+        $AdditionalPoints
+        )
     $data = @()
-#Test ping to 8.8.8.8  on Win10Client
-for ($i = 1; $i -le 24; $i++) {
-    $points = 0
-    $Win10_Ping8 = InvokeScript -SourceDevice "Win10Client" -Script "(Test-Connection 8.8.8.8 -Count 1).StatusCode" -Username "sysadmin" -Passwd "Change.me!"
-    Write-host("Testing Ping to 8.8.8.8 on Team_$i Win10Client")
-    if  ($Win10_Ping8 -match '0'){
-        Write-host("Team_$i Win10Client ping to 8.8.8.8 successful")
-        $points += 10
-    }else{
-        Write-host("Team_$i Win10Client ping to 8.8.8.8 unsucessful")
-    }
-    #Test ping to dns.google on Win10Client 
-    
-    $Win10_PingGoogle = InvokeScript -SourceDevice "Win10Client" -Script "(Test-Connection dns.google -Count 1).StatusCode" -Username "sysadmin" -Passwd "Change.me!"
-    Write-host("Testing Ping to dns.google on Team_$i Win10Client")
-    if  ($Win10_PingGoogle -match '0'){
-        Write-host("Team $i Win10Client ping to dns.google successful")
-        $points += 10
-    }else{
-        Write-host("Team $i Win10Client ping to dns.google unsuccessful")
-    }
-    #Test ping tot 8.8.8.8 on UbuntuClient
-    $Nix_Ping8 = InvokeScript -SourceDevice "UbuntuClient" -Script "ping -c 1 8.8.8.8 &> /dev/null; echo $?" -Username "sysadmin" -Passwd "Change.me!"
-    Write-host("Testing Ping to 8.8.8.8 on Team_$i UbuntuClient")
-    if  ($Nix_Ping8 -match 'True'){
-        Write-host("Team $i UbuntuClient ping to 8.8.8.8 successful")
-        $points += 10
-    }else{
-        Write-host("Team $i UbuntuClient ping to 8.8.8.8 unsuccessful")
-    }
-    #Test ping to dns.google on UbuntuClient
-    $Nix_PingGoogle = InvokeScript -SourceDevice "UbuntuClient" -Script "ping -c 1 dns.google &> /dev/null; echo $?" -Username "sysadmin" -Passwd "Change.me!"
-    Write-host("Testing Ping to dns.google on Team_$i UbuntuClient")
-    if  ($Nix_PingGoogle -match 'True'){
-        $points += 10
-        Write-host("Team $i UbuntuClient ping to dns.google successful")
-    }else{
-        Write-host("Team $i UbuntuClient ping to dns.google unsuccessful")
-    }
-    ##Should there be a check for installing updates on UbuntuClient?
-    $data += [pscustomobject]@{
-        Team = $i
-        Points = $points
+    for ($i = 1; $i -le 24; $i++) {
+        $ScriptResult = InvokeScript -SourceDevice $SourceDevice -Script $Script -Username $Username -Passwd $Passwd
+        $points = 0
+        if ($ScriptResult -match $TestExpectedResult) {
+            $points += $AdditionalPoints
+            Write-Output "$TestName successful for Team $i $SourceDevice"
+        } else {
+            Write-Output "$TestName unsuccessful for Team $i $SourceDevice"
+        }
+        $data += [pscustomobject]@{
+            Team = $i
+            Points = $points
+        }
     }
 }
-    Write-Host("Grading Homework 1 Complete!")
-    $endTime = Get-Date
-    $elapsedTime = $endTime - $startTime
-    Write-Host "This grading took: $($elapsedTime.TotalSeconds) seconds to run"
-    $data | Export-Csv -Path 'Homework1_Grades.csv' -NoTypeInformation
 
+CheckState -SourceDevice "Win10Client" -Script "(Test-Connection 8.8.8.8 -Count 1).StatusCode" -Username "sysadmin" -Passwd "Change.me!" -TestName "Pinging 8.8.8.8" -TestExpectedResult "0" -AdditionalPoints 5
 
+CheckState -SourceDevice "Win10Client" -Script "(Test-Connection dns.google -Count 1).StatusCode" -Username "sysadmin" -Passwd "Change.me!" -TestName "Pinging dns.google" -TestExpectedResult "0" -AdditionalPoints 5
 
+#CheckState -SourceDevice "UbuntuClient" -Script "ping -c 1 8.8.8.8 &> /dev/null; echo $?" -Username "sysadmin" -Passwd "Change.me!" -TestName "Pinging 8.8.8.8" -TestExpectedResult "True" -AdditionalPoints 5
+    
+#CheckState -SourceDevice "UbuntuClient" -Script "ping -c 1 dns.google &> /dev/null; echo $?" -Username "sysadmin" -Passwd "Change.me!" -TestName "Pinging dns.google" -TestExpectedResult "True" -AdditionalPoints 5
 
+$data | Export-Csv -Path 'Homework1_Grades.csv' -NoTypeInformation
+Write-Host("Grading Homework 1 Complete!")
+$endTime = Get-Date
+$elapsedTime = $endTime - $startTime
+Write-Host "This grading took: $($elapsedTime.TotalSeconds) seconds to run"
